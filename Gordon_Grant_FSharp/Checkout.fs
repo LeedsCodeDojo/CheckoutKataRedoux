008288c08966cb0967c7ec07fb9d1bc8d00d4ff7
Grant Crofton <grantac@hotmail.com>
2015-02-05 00:16:33 +0000

    Added F# solution

---
+++ Gordon_Grant_FSharp/Checkout.fs
@@ -0,0 +1,23 @@
+﻿module Checkout
+
+type Product = Apple | Beans | Coke | Deodorant
+
+let checkout (items:Product list) (prices:Map<Product,float>) (discounts:Map<Product list,float>) = 
+    
+    let (undiscountedItems, discountsTotal) =
+        discounts 
+        |> Map.fold (fun agg key value -> 
+                let remainingItems = (fst agg)
+                let discountProducts = key |> List.toArray
+                let discountedProductCount = key |> List.length
+
+                let discountFound = 
+                    (remainingItems |> Array.length >= discountedProductCount)
+                    && (remainingItems.[0..discountedProductCount-1] = discountProducts)
+
+                match discountFound with
+                | true -> (remainingItems.[discountedProductCount..]),(snd agg + value)
+                | _ -> agg)
+            (items |> List.toArray,0.0)
+
+    discountsTotal + (undiscountedItems |> Array.sumBy (fun item -> prices.[item]))
---
+++ Gordon_Grant_FSharp/Checkout_Tests.fs
@@ -0,0 +1,48 @@
+﻿module Checkout_Tests
+
+open Xunit
+open FsUnit.Xunit
+open Checkout
+
+// Apple 0.30  (discount 4 for 1.00)
+// Beans 0.50
+// Coke  1.80
+// Deodorant 2.50 (discount 2 for 4.50)
+
+let prices = 
+    Map.empty
+        .Add(Apple, 0.3)
+        .Add(Beans, 0.5)
+        .Add(Coke, 1.8)
+        .Add(Deodorant, 2.5)
+
+let appleDiscountedPrice = 1.0
+let deodorantDiscountedPrice = 4.5
+
+let discounts = 
+    Map.empty
+        .Add([Apple; Apple; Apple; Apple], appleDiscountedPrice)
+        .Add([Deodorant; Deodorant], deodorantDiscountedPrice)
+
+[<Fact>]
+let ``Single item gives correct price`` () = 
+    checkout [Apple] prices discounts |> should equal prices.[Apple]
+    checkout [Beans] prices discounts |> should equal prices.[Beans]
+    checkout [Coke] prices discounts |> should equal prices.[Coke]
+    checkout [Deodorant] prices discounts |> should equal prices.[Deodorant]
+
+[<Fact>]
+let ``Multiple single items give correct price`` () = 
+    checkout [Apple; Beans] prices discounts |> should equal (prices.[Apple] + prices.[Beans])
+
+[<Fact>]
+let ``Apple discount works`` () = 
+    checkout [Apple; Apple; Apple; Apple] prices discounts |> should equal appleDiscountedPrice
+
+[<Fact>]
+let ``Deodorant discount works`` () = 
+    checkout [Deodorant; Deodorant] prices discounts |> should equal deodorantDiscountedPrice
+
+[<Fact>]
+let ``Mix of discounted and non-discounted single product gives correct price`` () = 
+    checkout [Apple; Apple; Apple; Apple; Apple] prices discounts |> should equal (prices.[Apple] + appleDiscountedPrice)
